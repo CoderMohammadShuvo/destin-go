f4cfed6ab2334653d5639eeb1bcc0dccda7da641
Tasnuva Rahman <tasnuvarahman1996@gmail.com>
2021-03-21 06:54:33 -0700

    make blog

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,11 +1,28 @@
-import React from 'react';
+
+import React from "react";
+import { Card } from "react-bootstrap";
+import blogData from "../../FakeData/blogData";
+import "./Blog.css"
 
 const Blog = () => {
-    return (
-        <div>
-            <h1 style={{textAlign:"center",marginTop:"50px"}}>This is blog</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="blog-header">Destini<strong className="blog-header-strong">Go</strong> Blog</h1>
+      {blogData.map((blog) => {
+        const { title, news, date } = blog;
+        return (
+          <Card className="card-style">
+            <Card.Header>{date}</Card.Header>
+            <Card.Body>
+              <Card.Title>{title}</Card.Title>
+              <Card.Text>{news}</Card.Text>
+              <button className="card-button">Go somewhere</button>
+            </Card.Body>
+          </Card>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Blog;
